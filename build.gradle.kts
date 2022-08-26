b526eaca3e796e1d5e5b26ce47ccf106d7192b87
Vladimir Lavruk <vladimirlavruk@granular.ag>
2022-08-26 14:24:59 -0700

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+kotlin {
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(npm("highlight.js", "10.7.2"))
+                implementation(compose.web.core)
+                implementation(compose.runtime)
+            }
+        }
+    }
+}
+
+// a temporary workaround for a bug in jsRun invocation - see https://youtrack.jetbrains.com/issue/KT-48273
+// Fixes webpack-cli incompatibility by pinning the newest version.
+afterEvaluate {
+    rootProject.extensions.configure<NodeJsRootExtension> {
+        nodeVersion = "16.0.0"
+        versions.webpackCli.version = "4.10.0"
+    }
+}
